0800a86e491b8b1e109d75b734f61134cf9a16ac
tokey000 <tokey000>
2015-07-13 01:19:21 +0800

    plot2
    plot2

--- code/plot2.R
+++ code/plot2.R
@@ -4,15 +4,20 @@ unzip("household_power_consumption.zip")
 ##read the data into R
 pw <-read.table("household_power_consumption.txt",header=TRUE,sep=";")
 ##convert the Date and Time variables to Date/Time classes
-tt <- strptime(paste(pw[,1],pw[,2]), "%d/%m/%Y %H:%M:%OS")
+tt <- strptime(paste(pw[,1],pw[,2]), "%d/%m/%Y %H:%M:%OS",tz="GMT")
 pw2 <- cbind(pw,tt)
 
 ##set the range of date
-st <-  strptime('1/2/2007 00:00:00', "%d/%m/%Y %H:%M:%OS")
-et <-  strptime('3/2/2007 00:00:00', "%d/%m/%Y %H:%M:%OS")
+st <-  strptime('1/2/2007 00:00:00', "%d/%m/%Y %H:%M:%OS",tz="GMT")
+et <-  strptime('3/2/2007 00:00:00', "%d/%m/%Y %H:%M:%OS",tz="GMT")
 
 ##extract data data from the selected date
  ll <- pw2[which(pw2[,"tt"] >= st & pw2[,"tt"] < et),]
-png("plot1.png",width = 480, height = 480)
-hist(gap, col = "red",xlab="Global active power(kilowatts)",main="Global active power")
+tt<- (ll[,"tt"])
+gap<-as.numeric(ll[,"Global_active_power"])
+maxt<-max(tt)
+mint<-min(tt)
+midt <-  strptime('2/2/2007 00:00:00', "%d/%m/%Y %H:%M:%OS",tz="GMT")
+png("plot2.png",width = 480, height = 480)
+plot(tt,gap,xlab="",ylab="Global active power(kilowatts)",main="Global active power",type='l')
 dev.off()
