e34b6e42bf2e99abfa212aaaad538995bdbb35ea
tokey000 <tokey000>
2015-07-13 00:02:54 +0800

    plot1
    plot1

---
+++ code/plot1.R
@@ -0,0 +1,21 @@
+##download the data and unzip the data 
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile="household_power_consumption.zip")
+unzip("household_power_consumption.zip")
+
+##read the data into R
+pw <-read.table("household_power_consumption.txt",header=TRUE,sep=";")
+
+##convert the Date and Time variables to Date/Time classes
+tt <- strptime(paste(pw[,1],pw[,2]), "%d/%m/%Y %H:%M:%OS")
+pw2 <- cbind(pw,tt)
+
+##set the range of date
+st <-  strptime('1/2/2007 00:00:00', "%d/%m/%Y %H:%M:%OS")
+et <-  strptime('3/2/2007 00:00:00', "%d/%m/%Y %H:%M:%OS")
+
+##extract data data from the selected date
+ll <- pw2[which(pw2[,"tt"] >= st & pw2[,"tt"] < et),]
+
+hist(gap, col = "red",xlab="Global active power(kilowatts)",main="Global active power")
+png("plot1.png",width = 480, height = 480)
+dev.off()
